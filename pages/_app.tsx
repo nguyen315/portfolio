e769474e6ae64cd1f44faae912fa6d1768e3b50e
Nguyen Ho <hominhnguyen315@gmail.com>
2022-09-10 19:32:45 +0700

    [NEW] progressbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,12 @@ import Fonts from '@components/fonts'
 import theme from '@lib/theme'
 import { AnimatePresence } from 'framer-motion'
 import type { AppProps } from 'next/app'
+import NextNProgress from 'nextjs-progressbar'
 
 const Website = ({ Component, pageProps, router }: AppProps) => {
   return (
     <ChakraProvider theme={theme}>
+      <NextNProgress startPosition={0.2} />
       <Fonts />
       <Layout router={router}>
         <AnimatePresence exitBeforeEnter initial={true}>
